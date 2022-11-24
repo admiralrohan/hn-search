c7abded9f6bc481dc60f45cfddf5765bf7a3d542
Rohan Gayen <rohangayen@gmail.com>
2022-11-24 19:06:19 +0530

    refactor: `postLink` variable

--- src/components/HomePostItem.tsx
+++ src/components/HomePostItem.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 import styles from "../../styles/HomePostItem.module.css";
 
 export default function HomePostItem({ post }: any) {
+  const postLink = "/post/" + post.objectID;
+
   return (
     <li className={styles.post}>
       <div className={styles.post__title}>
-        <Link href={"/post/" + post.objectID} className={styles.post__name}>
+        <Link href={postLink} className={styles.post__name}>
           {post.title}
         </Link>{" "}
         (
@@ -16,12 +18,9 @@ export default function HomePostItem({ post }: any) {
       </div>
 
       <div className={styles.post__subtitle}>
-        <Link href={"/post/" + post.objectID}>{post.points} points</Link> |{" "}
-        {post.author} |{" "}
-        <Link href={"/post/" + post.objectID}>11 months ago</Link> |{" "}
-        <Link href={"/post/" + post.objectID}>
-          {post.num_comments} comments
-        </Link>
+        <Link href={postLink}>{post.points} points</Link> | {post.author} |{" "}
+        <Link href={postLink}>11 months ago</Link> |{" "}
+        <Link href={postLink}>{post.num_comments} comments</Link>
       </div>
     </li>
   );
