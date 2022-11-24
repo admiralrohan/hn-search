fe92f13a804e92bd13c283510a375d93925be03b
Rohan Gayen <rohangayen@gmail.com>
2022-11-24 19:43:14 +0530

    feat: dynamic time for post items

--- src/components/HomePostItem.tsx
+++ src/components/HomePostItem.tsx
@@ -1,8 +1,35 @@
 import Link from "next/link";
 import styles from "../../styles/HomePostItem.module.css";
+import { Hit } from "../interfaces/search-result";
 
-export default function HomePostItem({ post }: any) {
+export default function HomePostItem({ post }: { post: Hit }) {
   const postLink = "/post/" + post.objectID;
+
+  // Calculate relative time
+  const getRelativeTime = () => {
+    const postAge = +new Date() - post.created_at_i * 1000;
+
+    let formattedPostAge = postAge / 1000 / 60;
+    if (formattedPostAge < 1) return "Just posted";
+    if (formattedPostAge < 60)
+      return Math.round(formattedPostAge) + " minutes ago";
+
+    formattedPostAge /= 60;
+    if (formattedPostAge < 24)
+      return Math.round(formattedPostAge) + " hours ago";
+
+    formattedPostAge /= 24;
+    if (formattedPostAge < 30)
+      return Math.round(formattedPostAge) + " days ago";
+
+    formattedPostAge /= 30;
+    if (formattedPostAge < 12)
+      return Math.round(formattedPostAge) + " months ago";
+
+    formattedPostAge /= 12;
+    return Math.round(formattedPostAge) + " years ago";
+  };
+  let relativeTime = getRelativeTime();
 
   return (
     <li className={styles.post}>
@@ -19,7 +46,7 @@ export default function HomePostItem({ post }: any) {
 
       <div className={styles.post__subtitle}>
         <Link href={postLink}>{post.points} points</Link> | {post.author} |{" "}
-        <Link href={postLink}>11 months ago</Link> |{" "}
+        <Link href={postLink}>{relativeTime}</Link> |{" "}
         <Link href={postLink}>{post.num_comments} comments</Link>
       </div>
     </li>
